d73ee02c48ab2fdd4c82305f7065666e49756b42
Slowhand_47 <slowhand_47@gmx.de>
2017-01-22 18:39:43 +0100

    Only some formatting and comments

--- main.c
+++ main.c
@@ -3,8 +3,8 @@
 //* GRBL Z-AXIS SERVO ADAPTER           *
 //*                                     *
 //***************************************
-//* Description:						*
-//* 									*
+//* Description:                        *
+//*                                     *
 //* Simulates an axis, drives RC Servo. *
 //*                                     *
 //* Want to use a servo in GRBL for the *
@@ -21,161 +21,151 @@
 //*                                     *
 //* Great for use in existing hardware. *
 //* No fundamental changes in firmware  *
-//* are needed.  						*
-//*										*
+//* are needed.                         *
+//*                                     *
 //***************************************
-//* Pins:								*
-//*          ______						*
-//*   RESET-|      |-+5V				*
-//*  HOME_Z-|ATTiny|-STEP				*
-//*        -|25/85 |-DIR				*
-//*     GND-|______|-Servo signal		*
-//*										*
-//* Connect the parts to the respective	*
-//* pins. Set step direction to 		*
+//* Pins:                               *
+//*          ______                     *
+//*   RESET-|      |-+5V                *
+//*  HOME_Z-|ATTiny|-STEP               *
+//*        -|25/85 |-DIR                *
+//*     GND-|______|-Servo signal       *
+//*                                     *
+//* Connect the parts to the respective *
+//* pins. Set step direction to         *
 //* inverted in your Machine.           *
-//* 									*
+//*                                     *
 //***************************************
-
-
-
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
 
-
 #define SERVO_PIN PB0
 #define DIRECTION_PIN PB1
 #define HOME_PIN PB3
 
-//DO NOT CHANGE STEP_PIN
+// DO NOT CHANGE STEP_PIN (will trigger interrupt on signal edge)
 #define STEP_PIN PB2
 
-//Uncomment if you're using the Z-Axis for a pen and want to move it up when homing
+// Uncomment if you're using the Z-Axis for a pen and want to move it up
+// when homing
 #define REVERSE_Z_BEHAVIOUR
 
-
-//Experiment until you find the values for servo completely up or down.
-//Default values should work OK when using a pen and reversed Z axis
+// Experiment until you find the values for servo completely up or down.
+// Default values should work OK when using a pen and reversed Z axis.
 #define PWM_MIN 50
 #define PWM_MAX 400
 
-//Changes "sensivity" of servo movement per step
+// Servo movement per step
 #define PWM_RESOLUTION 20
+
+// Current pulse width setting
 volatile int pwm;
 
-
+// Block for the given amount of microseconds.
+// _delay_us() is limited to 768us/(F_CPU in MHz) = 96us @8MHz.
+// This function therefore breaks down the delay into several calls to
+// _delay_us().
 void delayus(int useconds)
 {
-for (int _temp=0;_temp<useconds;_temp++)
-{
-_delay_us(1);
-}
-
+  for (int _temp=0; _temp<useconds; _temp++)
+  {
+    _delay_us(1);
+  }
 }
 
 void init (void)
 {
-	//IO-Ports setup
-	DDRB |=(1<<SERVO_PIN) |(1<<HOME_PIN);
-	//PORTB |=(1<<DIRECTION_PIN)|(1<<STEP_PIN);
-	
-	//Falling-edge Interrupt on PB2
-	MCUCR |=(1<<ISC01);
-	GIMSK |=(1<<INT0);
-	
-	//Timer0 setup
-	TCCR0A |=(1<<WGM01); //CTC mode, set OC0A on match
-	TCCR0B |=(1<<CS02)|(1<<CS00);  //Prescaler 1024
-	
-	TIMSK |=(1<<OCIE0A);  //CTC Interrupt enable
-	
-	OCR0A= (8000000/(1024*50)-1);  //Set Timer to ~100Hz
-	
-	//Set Z-servo to max
-	pwm=PWM_MAX;
-	
+  //IO-Ports setup
+  DDRB |=(1<<SERVO_PIN) |(1<<HOME_PIN);
+  //PORTB |=(1<<DIRECTION_PIN)|(1<<STEP_PIN);
+    
+  //Falling-edge Interrupt on PB2
+  MCUCR |=(1<<ISC01);
+  GIMSK |=(1<<INT0);
+    
+  //Timer0 setup
+  TCCR0A |=(1<<WGM01); //CTC mode, set OC0A on match
+  TCCR0B |=(1<<CS02)|(1<<CS00);  //Prescaler 1024
+    
+  TIMSK |=(1<<OCIE0A);  //CTC Interrupt enable
+   
+  OCR0A= (8000000/(1024*50)-1);  //Set Timer to ~100Hz
+    
+  //Set Z-servo to max
+  pwm=PWM_MAX;  
 }
-
 
 
 int main (void)
 {
-	init();
-	sei();
-	while(1)
-	{
-	
-		#ifdef REVERSE_Z_BEHAVIOUR
-		
-		if (pwm<=PWM_MIN)
-		{
-			pwm=PWM_MIN;
-			
-		}
-		
-		if (pwm <PWM_MAX)
-		{
-			
-			PORTB |=(1<<HOME_PIN);
-		}
-		
-		if (pwm>=PWM_MAX)
-		{
-			pwm=PWM_MAX;
-			PORTB &= ~(1<<HOME_PIN);
-		}
-	
-		#else 
-		if (pwm<=PWM_MIN)
-		{
-			pwm=PWM_MIN;
-			PORTB &= ~(1<<HOME_PIN);
-		}
-		
-		if (pwm >PWM_MIN)
-		{
-			
-			PORTB |=(1<<HOME_PIN);
-		}
-		
-		if (pwm>=PWM_MAX)
-		{
-			pwm=PWM_MAX;
-			
-		}
-
-		#endif
-	}
-
-	return 0;
+  init();
+  sei();
+  while(1)
+  {
+    #ifdef REVERSE_Z_BEHAVIOUR
+        
+    if (pwm<=PWM_MIN)
+    {
+      pwm=PWM_MIN;
+    }
+    
+    if (pwm <PWM_MAX)
+    {
+      PORTB |=(1<<HOME_PIN);
+    }
+    
+    if (pwm>=PWM_MAX)
+    {
+      pwm=PWM_MAX;
+      PORTB &= ~(1<<HOME_PIN);
+    }
+    #else 
+    if (pwm<=PWM_MIN)
+    {
+      pwm=PWM_MIN;
+      PORTB &= ~(1<<HOME_PIN);
+    }
+        
+    if (pwm >PWM_MIN)
+    {
+      PORTB |=(1<<HOME_PIN);
+    }
+    
+    if (pwm>=PWM_MAX)
+    {
+      pwm=PWM_MAX;
+    }
+    #endif
+  }
+  return 0;
 }
 
-//Software PWM
+// Software PWM
 ISR(TIMER0_COMPA_vect)
 {
-PORTB |=(1<<SERVO_PIN); //Servo pin on
+  PORTB |=(1<<SERVO_PIN); //Servo pin on
 
-_delay_us(550); 		//Wait for PWM to finish	
-delayus(pwm);
+  _delay_us(550);         //Wait for PWM to finish    
+  delayus(pwm);
 
-PORTB &=~(1<<SERVO_PIN); //Servo pin off
-
+  PORTB &=~(1<<SERVO_PIN); //Servo pin off
 }
 
 
-//Falling edge interrupt
+// Falling edge interrupt
+// Count step, depending on direction setting.
 ISR(INT0_vect)
 {
-	if (!(PINB & (1<< DIRECTION_PIN))) 
-	{
-		pwm-=PWM_RESOLUTION;
-	}
-	else 
-	{
-		pwm+=PWM_RESOLUTION;
-	}
+  if (!(PINB & (1<< DIRECTION_PIN))) 
+  {
+    pwm-=PWM_RESOLUTION;
+  }
+  else 
+  {
+    pwm+=PWM_RESOLUTION;
+  }
 }
 
 
